af76b8558ad71aaa735f7d0f068336e00b7fde2b
DeividsonOmedio <deividsonomedio@outlook.com>
2023-04-21 21:46:48 -0300

    Atualizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,10 @@ function updateProfileInfo(profileData) {
 }
 
 function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skilss.dotnetskills')
+    softSkills.innerHTML = profileData.skills.dotnetskills.map(skill => `<li>${skill}</li>`).join('')
+}
+function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
